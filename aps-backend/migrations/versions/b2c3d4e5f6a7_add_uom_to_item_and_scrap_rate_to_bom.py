a79dc30260d8634cf1b132b4c28f92e8bda7bedb
Tran Van Tuan Phong <tvtp11052002@gmail.com>
2026-07-21 15:51:10 +0700

    fix(be): rename migration to resolve duplicate revision id

--- aps-backend/migrations/versions/b2c3d4e5f6a7_add_uom_to_item_and_scrap_rate_to_bom.py
+++ aps-backend/migrations/versions/b2c3d4e5f6a7_add_uom_to_item_and_scrap_rate_to_bom.py
@@ -4,16 +4,16 @@ Master fields needed by the FE master views (Items, BOM). G-System sync does not
 provide either yet, so both are nullable with sane defaults (uom='EA', scrap_rate=0)
 and can be backfilled later.
 
-Revision ID: a1b2c3d4e5f6
-Revises: f9c3a7b1d2e6
+Revision ID: b2c3d4e5f6a7
+Revises: a1b2c3d4e5f6
 Create Date: 2026-07-21 13:25:00.000000
 """
 from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-revision: str = 'a1b2c3d4e5f6'
-down_revision: Union[str, Sequence[str], None] = 'f9c3a7b1d2e6'
+revision: str = 'b2c3d4e5f6a7'
+down_revision: Union[str, Sequence[str], None] = 'a1b2c3d4e5f6'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
